90f3d7d68eeacf3b685d5ae7ce41c5cdefe98892
glassp <otakupasi@gmail.com>
2021-01-23 14:01:44 +0100

    imporvements on copy module

--- lib/modules/copy/CopyModule.dart
+++ lib/modules/copy/CopyModule.dart
@@ -5,6 +5,7 @@ import 'package:taida/core/config/ConfigurationLoader.dart';
 import 'package:taida/core/log/LogLabel.dart';
 import 'package:taida/core/log/Logger.dart';
 import 'package:taida/modules/Module.dart';
+import 'package:taida/modules/copy/DirectoryCopy.dart';
 import 'package:watcher/watcher.dart';
 
 class CopyModule extends Module {
@@ -26,37 +27,10 @@ class CopyModule extends Module {
     var moduleConfig = config.moduleConfiguration['copy'];
     Logger.debug('Running Module $name with configuration $moduleConfig');
     for (var task in moduleConfig) {
-      var destination = Directory(config.projectRoot + '/' + task['to']);
+      var destination = Directory(config.outputDirectory + '/' + task['to']);
       for (var from in task['from']) {
         var source = Directory(config.projectRoot + '/' + from);
-        await _copy(source, destination);
-      }
-    }
-  }
-
-  /// Recursive copy of all Dirs and Files in `source` to `destination`
-  void _copy(Directory source, Directory destination) async {
-    if (!await source.exists()) {
-      Logger.warn('${source.path} does not exist. Skipping...');
-      return;
-    }
-    var elements = source.listSync();
-    for (var entity in elements) {
-      var name = entity.path.split('/').last;
-      if (entity is Directory) {
-        Logger.verbose(
-            'Copying file ${source.path}/$name to ${destination.path}/$name');
-        var subdir = Directory(destination.path + '/' + name);
-        await subdir.create(recursive: true);
-        await _copy(entity, subdir);
-      } else if (entity is File) {
-        if (!await destination.exists()) {
-          await destination.create(recursive: true);
-        }
-        Logger.verbose(
-            'Copying file ${source.path}/$name to ${destination.path}/$name');
-        var file = File(destination.path + '/' + name);
-        await entity.copy(file.path);
+        await DirectoryCopy.copy(source, destination);
       }
     }
   }
---
+++ lib/modules/copy/DirectoryCopy.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:taida/core/log/Logger.dart';
+
+class DirectoryCopy {
+  /// Recursive copy of all Dirs and Files in `source` to `destination`.
+  /// Also returns a list of all files that have been copied
+  static Future<List<File>> copy(Directory source, Directory destination) async {
+    if (!await source.exists()) {
+      Logger.warn('${source.path} does not exist. Skipping...');
+      return [];
+    }
+    var files = <File>[];
+    var elements = source.listSync();
+    for (var entity in elements) {
+      var name = entity.path.split('/').last;
+      if (entity is Directory) {
+        Logger.verbose(
+            'Copying file ${source.path}/$name to ${destination.path}/$name');
+        var subdir = Directory(destination.path + '/' + name);
+        await subdir.create(recursive: true);
+        var results = await copy(entity, subdir);
+        files.addAll(results);
+      } else if (entity is File) {
+        if (!await destination.exists()) {
+          await destination.create(recursive: true);
+        }
+        Logger.verbose(
+            'Copying file ${source.path}/$name to ${destination.path}/$name');
+        var file = File(destination.path + '/' + name);
+        files.add(file);
+        await entity.copy(file.path);
+      }
+    }
+    return files;
+  }
+}
